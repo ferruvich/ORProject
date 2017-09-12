6c36a8a72eb2f73a239da8534752eb6f155c2189
OPTC-Agenda <ibbus93@gmail.com>
2017-09-12 16:35:13 +0200

    Tiny changes

--- src/Euristica2Fasi.java
+++ src/Euristica2Fasi.java
@@ -15,41 +15,38 @@ public class Euristica2Fasi {
     private static final FilenameFilter filenameFilter = (dir, name) -> name.contains(".json");
 
     public static void main(String[] args) {
-        for (File f : new File("InstancesJSON").listFiles(filenameFilter)) {
-            String fileName = f.getPath().replace("\\", "/");
+        String fileName = "InstancesJSON/A1.json";
 
-            ExecutorService executor = Executors.newFixedThreadPool(8);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
 
-            List algorithmOneFutures = new ArrayList<>();
-            List algorithmsOne = new ArrayList<>();
-            List algorithmTwoFutures = new ArrayList<>();
-            List algorithmsTwo = new ArrayList<>();
+        List algorithmOneFutures = new ArrayList<>();
+        List algorithmsOne = new ArrayList<>();
+        List algorithmTwoFutures = new ArrayList<>();
+        List algorithmsTwo = new ArrayList<>();
 
-            for (int i = 0; i < NUMBER_OF_ITERATION; i++) {
-                TSPInstance in = TSPInstance.getInstance(fileName);
-                DistanceMatrix.getInstance().initialize(in);
-                Algorithm algorithmOne = new Algorithm(in, Algorithm.ALGORITHM_ONE, "Iteration 1");
-                algorithmsOne.add(algorithmOne)
-                ;
-                FutureTask futureTask = new FutureTask<>(algorithmOne);
-                algorithmOneFutures.add(futureTask);
-                executor.execute(futureTask);
-            }
-            for (int i = 0; i < NUMBER_OF_ITERATION; i++) {
-                TSPInstance in = TSPInstance.getInstance(fileName);
-                DistanceMatrix.getInstance().initialize(in);
-                Algorithm algorithmTwo = new Algorithm(in, Algorithm.ALGORITHM_TWO, "Iteration 2");
-                algorithmsTwo.add(algorithmTwo);
-                FutureTask futureTask = new FutureTask<>(algorithmTwo);
-                algorithmTwoFutures.add(futureTask);
-                executor.execute(futureTask);
-            }
-            getBestRouteList("Algorithm One", algorithmOneFutures, algorithmsOne, fileName);
-            getBestRouteList("Algorithm Two", algorithmTwoFutures, algorithmsTwo, fileName);
-
-            executor.shutdown();
+        for (int i = 0; i < NUMBER_OF_ITERATION; i++) {
+            TSPInstance in = TSPInstance.getInstance(fileName);
+            DistanceMatrix.getInstance().initialize(in);
+            Algorithm algorithmOne = new Algorithm(in, Algorithm.ALGORITHM_ONE, "Iteration 1");
+            algorithmsOne.add(algorithmOne)
+            ;
+            FutureTask futureTask = new FutureTask<>(algorithmOne);
+            algorithmOneFutures.add(futureTask);
+            executor.execute(futureTask);
         }
+        for (int i = 0; i < NUMBER_OF_ITERATION; i++) {
+            TSPInstance in = TSPInstance.getInstance(fileName);
+            DistanceMatrix.getInstance().initialize(in);
+            Algorithm algorithmTwo = new Algorithm(in, Algorithm.ALGORITHM_TWO, "Iteration 2");
+            algorithmsTwo.add(algorithmTwo);
+            FutureTask futureTask = new FutureTask<>(algorithmTwo);
+            algorithmTwoFutures.add(futureTask);
+            executor.execute(futureTask);
+        }
+        getBestRouteList("Algorithm One", algorithmOneFutures, algorithmsOne, fileName);
+        getBestRouteList("Algorithm Two", algorithmTwoFutures, algorithmsTwo, fileName);
 
+        executor.shutdown();
     }
 
     private static void getBestRouteList(String algorithmName, List<FutureTask<Pair<RouteList, RouteList>>> algorithmFutures, List<Algorithm> algorithms,  String fileName) {
